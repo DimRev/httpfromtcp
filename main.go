8f6764989a89fce45ff145d8afa231cfe731158a
DimRev <dimarev444@gmail.com>
2025-04-06 21:10:32 +0300

    chore(tcp): Setup TCP reader

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"io"
 	"log"
-	"os"
+	"net"
 	"strings"
 )
 
@@ -12,36 +12,60 @@ const PATH_TO_FILE = "./messages.txt"
 const BUFFER_SIZE = 8
 
 func main() {
-	file, err := os.Open(PATH_TO_FILE)
+	// file, err := os.Open(PATH_TO_FILE)
+	// if err != nil {
+	// 	log.Fatalf("Error opening file: %v", err)
+	// }
+	// defer file.Close()
+
+	listener, err := net.Listen("tcp", ":42069")
 	if err != nil {
-		log.Fatalf("Error opening file: %v", err)
+		log.Fatalf("Error listening: %v", err)
 	}
-	defer file.Close()
+	defer listener.Close()
+	conn, err := listener.Accept()
+	if err != nil {
+		log.Fatalf("Error accepting: %v", err)
+	}
+	defer conn.Close()
 
-	b := make([]byte, BUFFER_SIZE)
-	currentLine := ""
+	linesCh := getLinesChannel(conn)
 
-	for {
-		n, err := file.Read(b)
-		if err != nil && err != io.EOF {
-			log.Fatalf("Error reading file: %v", err)
-		}
-		if n > 0 {
-			currentLine += string(b[:n])
-		}
+	for line := range linesCh {
+		fmt.Printf("read: %s\n", line)
+	}
+}
 
-		splitLines := strings.Split(currentLine, "\n")
-		for i := 0; i < len(splitLines)-1; i++ {
-			fmt.Printf("read: %s\n", splitLines[i])
-		}
-		currentLine = splitLines[len(splitLines)-1]
-
-		if err == io.EOF {
-			if currentLine != "" {
-				fmt.Printf("read: %s\n", currentLine)
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	lineCh := make(chan string)
+	go func() {
+		b := make([]byte, BUFFER_SIZE)
+		currentLine := ""
+		for {
+			n, err := f.Read(b)
+			if err != nil && err != io.EOF {
+				close(lineCh)
+				log.Fatalf("Error reading file: %v", err)
 			}
-			break
-		}
-	}
+			if n > 0 {
+				currentLine += string(b[:n])
+			}
 
+			splitLines := strings.Split(currentLine, "\n")
+			for i := 0; i < len(splitLines)-1; i++ {
+				lineCh <- splitLines[i]
+			}
+			currentLine = splitLines[len(splitLines)-1]
+
+			if err == io.EOF {
+				if currentLine != "" {
+					lineCh <- currentLine
+				}
+				close(lineCh)
+				break
+			}
+		}
+	}()
+
+	return lineCh
 }
